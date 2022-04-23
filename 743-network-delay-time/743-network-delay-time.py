129cc658dae4cc15618f5ab87e66732c85e335f1
MohitTare <mohittare92@gmail.com>
2022-04-23 15:58:33 +0530

    Time: 590 ms (55.51%), Space: 16.6 MB (49.08%) - LeetHub

---
+++ 743-network-delay-time/743-network-delay-time.py
@@ -0,0 +1,27 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+        
+        for e1,e2,w in times:
+            graph[e1].append((e2,w))
+        
+        elapsed_time = [0] + [float('inf')] * n
+        queue = deque([(0,k)])
+
+        def bfs():
+            while queue:
+                t,curr = queue.popleft()
+                if t < elapsed_time[curr]:
+                    elapsed_time[curr] = t
+                    for v,w in graph[curr]:
+                        queue.append((t+w,v))
+                        
+       
+        bfs()
+        result = max(elapsed_time)
+        return result if result < float('inf') else -1
+                    
+                
+        
+        
+        
