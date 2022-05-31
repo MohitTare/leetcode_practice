8ea6a67397b0069f41170e907f9d4eca27af71d9
MohitTare <mohittare92@gmail.com>
2022-05-31 19:33:33 +0530

    Time: 336 ms (84.39%), Space: 28.5 MB (49.46%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -1,19 +1,27 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2 != 0:
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
             return False
+            
+        target_sum = total_sum // 2
         
-        dp = set()
-        dp.add(0)#base case
+        dp = {}
+        def dfs(i,target_sum):
+            if i >= len(nums) or target_sum < 0:
+                return False
+            
+            if (i,target_sum) in dp:
+                return dp[(i,target_sum)]
+            
+            dp[(i,target_sum)] = False
+            if target_sum == 0:
+                dp[(i,target_sum)] = True
+                return dp[(i,target_sum)] 
+            
+            dp[(i,target_sum)] =  dfs(i + 1, target_sum - nums[i]) or dfs(i + 1, target_sum)
+            return dp[(i,target_sum)]
         
-        target = sum(nums) // 2
-        
-        for i in range(len(nums) - 1, -1,-1):
-            nextDp = set()
-            for t in dp:
-                nextDp.add(t + nums[i])
-                nextDp.add(t)
-            dp = nextDp
-        
-        return True if target in dp else False
-        
+        return dfs(0,target_sum)
+            
+            
