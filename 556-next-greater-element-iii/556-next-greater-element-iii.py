e14e01024d5193f8cfb776101341607c2f500d14
MohitTare <mohittare92@gmail.com>
2022-06-23 10:09:45 +0530

    Time: 53 ms (22.86%), Space: 14 MB (23.29%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.py
+++ 556-next-greater-element-iii/556-next-greater-element-iii.py
@@ -1,35 +1,19 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
-        heap = []
-        heapq.heapify(heap)
-        n = str(n)
-        i = len(n) - 1
-        while i > 0:
-            heapq.heappush(heap, n[i])
-            if n[i] > n[i-1]:
-                break
+        digits = list(str(n))
+        i = len(digits) - 1
+        while i-1 >= 0 and digits[i] <= digits[i-1]:
             i -= 1
-        # n is reverse sorted
-        if i == 0:
-            return -1
-        # Before this index, nothing needs to be changed.
-        i -= 1
-        pivotItem = n[i]
-        n = n[:i]
-        temp = ''
-        while heap:
-            '''
-                Build the string of numbers in ascending order till the last number is less than or equal to pivot number.
-                Once a number is found that is greater than pivot, append it to original string and then append the remaining in sorted order.
-            '''
             
-            poppedNum = heapq.heappop(heap)
-            if pivotItem == '' or poppedNum <= pivotItem:
-                temp += poppedNum
-            else:
-                n += poppedNum + temp + pivotItem
-                pivotItem = ''
-                temp = ''
-        n += temp
-        n = int(n)
-        return n if (1 <= n <= (2**31) - 1) else -1
+        if i == 0: return -1
+        
+        j = i
+        while j+1 < len(digits) and digits[j+1] > digits[i-1]:
+            j += 1
+        
+        digits[i-1], digits[j] = digits[j], digits[i-1]
+        digits[i:] = digits[i:][::-1]
+        ret = int(''.join(digits))
+        
+        return ret if ret < 1<<31 else -1
+        
