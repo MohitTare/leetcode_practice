3a043289cb8a965441e1ae957b43c7cc53aca763
MohitTare <mohittare92@gmail.com>
2022-07-03 15:19:18 +0530

    Time: 1112 ms (73.29%), Space: 20.2 MB (60.64%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,18 +1,22 @@
 class Solution:
     def totalFruit(self, fruit: List[int]) -> int:
-        fruitd = defaultdict(lambda:0)
+        dict_fruits = defaultdict(lambda:0)
         l = 0
-        count = 0
         for r in range(len(fruit)):
-            fruitd[fruit[r]] += 1
-            count += 1
-            if len(fruitd) > 2:
-                fruitd[fruit[l]] -= 1
-                if fruitd[fruit[l]] == 0: del fruitd[fruit[l]]
+            dict_fruits[fruit[r]] += 1
+            if len(dict_fruits) > 2:
+                dict_fruits[fruit[l]] -= 1
+                if dict_fruits[fruit[l]] == 0: del dict_fruits[fruit[l]]
                 l+=1
-        return r-l+1
+        return r - l +1
+            
+                    
                 
-        
+                
+                
+    
+    
+    
                 
                 
                 
