a71a4741d7e0103320b310d3462d24a20de2ece9
MohitTare <mohittare92@gmail.com>
2022-07-07 09:46:12 +0530

    Time: 342 ms (62.64%), Space: 14.6 MB (82.64%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.py
@@ -1,34 +1,17 @@
 class Solution:
     def balancedString(self, s: str) -> int:
-        counter = collections.Counter(s)
-        n = len(s) // 4
-        extras = {}
-        for key in counter:
-            if counter[key] > n:
-                extras[key] = counter[key] - n
-        
-        if not extras: return 0
-        i = 0
+        count = collections.Counter(s)
+        req_len = len(s) // 4
+        l = 0
         res = len(s)
-        for j in range(len(s)):
-            if s[j] in extras:
-                extras[s[j]] -= 1
+        for r in range(len(s)):
+            #print(l,r)
+            count[s[r]] -= 1
             
-            while max(extras.values()) <= 0:
-                res = min(res, j-i+1)
-                if s[i] in extras:
-                    extras[s[i]] += 1
-                i += 1
-                
-                
+            while l < len(s) and count['Q'] <= req_len and count['W'] <= req_len and count['E'] <= req_len and count['R'] <= req_len:
+                res = min(res,r - l + 1)
+                count[s[l]] += 1
+                l += 1
+            
         return res
-        
-        
-        
-        
-        
-        
-        
-        
-        
         
