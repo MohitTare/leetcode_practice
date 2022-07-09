69f92cab60f75292af47a73313d3aee471065bd5
MohitTare <mohittare92@gmail.com>
2022-07-09 18:49:49 +0530

    Time: 308 ms (92.79%), Space: 14.9 MB (74.28%) - LeetHub

--- 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
+++ 930-binary-subarrays-with-sum/930-binary-subarrays-with-sum.py
@@ -1,29 +1,24 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-    
         l = 0
-        count = curr_sum = res = 0
-
-        for r,num in enumerate(nums):
+        res = 0
+        count = 0
+        curr_sum = 0
+        for r in range(len(nums)):
             curr_sum += nums[r]
-
-            if num == 1:
+            
+            if nums[r] == 1:
                 count = 0
-
+                
             while l <= r and curr_sum >= goal:
                 if curr_sum == goal:
                     count += 1
-
+                
                 curr_sum -= nums[l]
                 l+=1
-
-            res += count
-
+            res +=count
+            
         return res
-
-    
-                    
-                
             
         
 
